862ebda7614b0e43a3da9febd32595883424c9bb
Mitzi Vite <vit20001@byui.edu>
2024-06-01 16:54:45 -0600

    error handler

--- server.js
+++ server.js
@@ -12,13 +12,19 @@ const app = express()
 const static = require("./routes/static")
 const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
+const utilities = require("./utilities"); 
 
 
 
 app.use(static)
 //Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 app.use("/inv", inventoryRoute)
+
+
+app.use(async (req, res, next) => {
+  next({status: 404, message:''})
+})
 
 // app.get("/", function(req, res){
 //   res.render("index", {title: "Home" })
@@ -29,6 +35,27 @@ app.use("/inv", inventoryRoute)
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+
+
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+
 
 /* ***********************
  * Local Server Information
